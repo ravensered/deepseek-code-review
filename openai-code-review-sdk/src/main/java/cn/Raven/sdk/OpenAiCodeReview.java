31a3d64761d5f001b196dec5cbecc8afc62508b0
Raven <1918039454@qq.com>
2025-02-09 21:12:07 +0800

    feat：init-project

--- openai-code-review-sdk/src/main/java/cn/Raven/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/cn/Raven/sdk/OpenAiCodeReview.java
@@ -5,8 +5,12 @@ package cn.Raven.sdk;
  * @Author Raven
  * @Date 2025/2/9
  */
+
 public class OpenAiCodeReview {
+
     public static void main(String[] args) {
-        System.out.println("测试执行了");
+        System.out.println("测试执行");
     }
+
 }
+
