4e9d6c577120ba3ec641bd7d2b2180037ff07b3d
Raven <1918039454@qq.com>
2025-02-10 15:20:06 +0800

    feat：第3节：Maven 构建和执行脚本-检出代码

--- openai-code-review-sdk/src/main/java/cn/Raven/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/cn/Raven/sdk/OpenAiCodeReview.java
@@ -1,5 +1,9 @@
 package cn.Raven.sdk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
 /**
  * @Description
  * @Author Raven
@@ -8,8 +12,27 @@ package cn.Raven.sdk;
 
 public class OpenAiCodeReview {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("测试执行");
+
+        // 1. 代码检出
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        processBuilder.directory(new File("."));
+
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+
+        StringBuilder diffCode = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            diffCode.append(line);
+        }
+
+        int exitCode = process.waitFor();
+        System.out.println("Exited with code:" + exitCode);
+
+        System.out.println("评审代码：" + diffCode.toString());
     }
 
 }
