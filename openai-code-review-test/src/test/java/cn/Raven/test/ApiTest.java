d364f3b53f3c681bc49bc458d8abbcf911bc4881
Raven <1918039454@qq.com>
2025-02-12 16:41:10 +0800

    feat：  第7节：工程重构，增加可维护性V1

--- openai-code-review-test/src/test/java/cn/Raven/test/ApiTest.java
+++ openai-code-review-test/src/test/java/cn/Raven/test/ApiTest.java
@@ -17,7 +17,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class ApiTest {
     @Test
     public void test(){
-        System.out.println("Raven");
+        System.out.println("Raven11111");
 
     }
 }
